29366587ced2663fba0074cf490f25ffb29b6cd9
jbarnell <jonathanbarnell@gmail.com>
2022-02-03 17:12:58 -0600

    fully working project

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,18 @@
+
+
 const functions = require("firebase-functions");
 const admin = require("firebase-admin") ;
+const { DotEnv } = require('dotenv').config();
 admin.initializeApp() ;
 
-<script type="text/javascript" src="/home/jbarnell/twitter-helper/key.js"></script>
-
-
-const YOUR_CLIENT_ID = data[0].clientId;
-const YOUR_CLIENT_SECRET = data[0].clientSecret;
+const YOUR_CLIENT_ID = process.env.clientId;
+const YOUR_CLIENT_SECRET = process.env.clientSecret;
 
 const dbRef = admin.firestore().doc('tokens/demo') ;
 
+
 const TwitterApi = require('twitter-api-v2').default;
+const { OpenAIApiAxiosParamCreator } = require("openai");
 const twitterClient = new TwitterApi({
     clientId: YOUR_CLIENT_ID,
     clientSecret: YOUR_CLIENT_SECRET,
@@ -18,21 +20,19 @@ const twitterClient = new TwitterApi({
 
 const callbackURL = 'http://127.0.0.1:5000/ai-twitter-bot/us-central1/callback'
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+    organization: process.env.aiOrg,
+    apiKey: process.env.aiApiKey,
+});
+const openai = new OpenAIApi(configuration);
 
 // step 1
-exports.auth = functions.https.onRequest((request, response) => {
+exports.auth = functions.https.onRequest( async (request, response) => {
 
     const { url, codeVerifier, state } = twitterClient.generateOAuth2AuthLink(
         callbackURL,
-        { scope: ['tweet.read', 'tweet.write', 'users.read', 'offline.access']}
+        { scope: ['tweet.read', 'tweet.write', 'users.read', 'offline.access'] }
     );
 
     //store verifier
@@ -44,7 +44,7 @@ exports.auth = functions.https.onRequest((request, response) => {
 
 
 // step 2
-exports.callback = functions.https.onRequest((request, response) => {
+exports.callback = functions.https.onRequest( async (request, response) => {
 
     const { state, code } = request.query;
 
@@ -73,5 +73,31 @@ exports.callback = functions.https.onRequest((request, response) => {
 }) ;
 
 
-// step 3
-exports.tweet = functions.https.onRequest((request, response) => {}) ;
+// // step 3
+exports.tweet = functions.https.onRequest( async (request, response) => {
+    const { refreshToken } = (await dbRef.get()).data();
+  
+    const {
+      client: refreshedClient,
+      accessToken,
+      refreshToken: newRefreshToken,
+    } = await twitterClient.refreshOAuth2Token(refreshToken);
+  
+    await dbRef.set({ accessToken, refreshToken: newRefreshToken });
+  
+    const nextTweet = await openai.createCompletion('text-davinci-001', {
+      prompt: 'tweet something cool for #techtwitter',
+      max_tokens: 64,
+    });
+  
+    const { data } = await refreshedClient.v2.tweet(
+      nextTweet.data.choices[0].text
+    );
+  
+    response.send(data);
+  });
+
+
+  const prompts = [
+
+  ]
